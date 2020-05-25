a84c49f8c79db48ed74f19c4f33665459ef7b726
thomaslnx <marcos.msilva10@gmail.com>
2020-05-25 20:56:36 -0300

    Added Google HTML file for SEO

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ function App() {
           name="google-site-verification"
           content="Gx1EvYN_thT7bHiq0b2kVFiocB6RbTetFoemmA1hG-8"
         />
-        link =
-        {[{ rel: 'canonical', href: 'https://thomaslnx-vuttr.netlify.app/' }]}
+        <link rel="canonical" href="https://thomaslnx-vuttr.netlify.app/" />
       </Helmet>
       <GlobalStyles />
       <Routes />
