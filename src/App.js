6f86f9c957f7bd6696dfe77807b1245e5d764f43
thomaslnx <marcos.msilva10@gmail.com>
2020-05-25 19:37:12 -0300

    SEO customization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Routes from './routes';
 import GlobalStyles from './styles/global';
@@ -6,6 +7,13 @@ import GlobalStyles from './styles/global';
 function App() {
   return (
     <>
+      <Helmet>
+        <title>VUTTR - Very Useful Tool To Remember</title>
+        <meta
+          name="description"
+          content="Bossabox's pro-lancer application project"
+        />
+      </Helmet>
       <GlobalStyles />
       <Routes />
     </>
